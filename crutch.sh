63f0f4517dd331009e8519a1ad6ba8664e077104
Rouben <rouben@rouben.net>
2014-05-08 21:27:13 -0400

    Parameterized and added support for multiple services

--- crutch.sh
+++ crutch.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
-cd /home/rouben
-SERVICES=( "bfgminer" "cgminer" "cpuminer" )
-#SERVICES=( "bfgminer" )
-for SERVICE in "${SERVICES[@]}"
+#!/bin/sh
+SERVICES="bfgminer cgminer cpuminer"
+OUTDIR="/run/shm/"
+LOGFILE=$OUTDIR"crutch.log"
+BINDIR="/usr/local/bin/"
+
+###############################################
+
+for SERVICE in $SERVICES
 do
-  #echo "Checking ${SERVICE}..."
-  pgrep $SERVICE &> /dev/null
-  result=$?
-  #echo "exit code: ${result}"
-  if [ "${result}" -ne "0" ] ; then
-    echo "`date`: $SERVICE is not running" >> /home/rouben/crutch.log
-    screen -dmS $SERVICE /usr/local/bin/$SERVICE
+  pgrep $SERVICE > /dev/null 2> /dev/null
+  TEST_RESULT=$?
+  if [ "${TEST_RESULT}" -ne "0" ]
+  then
+    echo "$(date +'%Y-%m-%d %T %Z'): $SERVICE is not running" >> $LOGFILE
+    screen -dmS $SERVICE $BINDIR$SERVICE
+    echo "Restarted $SERVICE at $(date +'%Y-%m-%d %T %Z')..." > $OUTDIR$SERVICE.status
+  else
+    screen -S $SERVICE -p 0 -X hardcopy $OUTDIR$SERVICE.status
   fi
 done
